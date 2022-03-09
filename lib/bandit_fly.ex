7e8a250e0bb77d0a9a120d630c08bdcc5f960774
Kentaro Kuribayashi <kentarok@gmail.com>
2022-03-09 17:43:51 +0900

    mix new bandit_fly --sup

---
+++ lib/bandit_fly.ex
@@ -0,0 +1,18 @@
+defmodule BanditFly do
+  @moduledoc """
+  Documentation for `BanditFly`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BanditFly.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
